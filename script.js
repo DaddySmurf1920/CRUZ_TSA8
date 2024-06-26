4792e6f20485367ebfdeffb8009f15594596def6
DaddySmurf <163823754+DaddySmurf1920@users.noreply.github.com>
2024-06-27 07:40:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+function updateFlexbox() {
+    const direction = document.getElementById('direction').value;
+    const justifyContent = document.getElementById('justify').value;
+    const alignItems = document.getElementById('align').value;
+
+    const flexContainer = document.getElementById('flex-container');
+    flexContainer.style.flexDirection = direction;
+    flexContainer.style.justifyContent = justifyContent;
+    flexContainer.style.alignItems = alignItems;
+}
+
+window.onload = () => {
+    updateFlexbox();
+};
